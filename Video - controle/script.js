68bf70ded3465966a02e46d06aa485613def2e35
Nathan Martini Duarte <77181059+NMD01@users.noreply.github.com>
2021-06-28 18:30:42 -0300

    Add files via upload

--- Video - controle/script.js
+++ Video - controle/script.js
@@ -16,11 +16,11 @@ function play(){
         video.play()
         
         play.src = "./imagens/pause.png"
-        play.style = " width:52px; transform: translateY(calc(-100% + 3px));"
+        play.style = " width:55px;"/* transform: translateY(calc(-100% + 3px)); */
     }else{
         play.teste = undefined
         play.src = "./imagens/play.png"
-        play.style = "width: 70px"
+        play.style = "width: 55px"
         video.pause()
     }
     
@@ -32,7 +32,7 @@ function stop(){
     
     let play = document.getElementById("play")
     play.src = "./imagens/play.png"
-    play.style = "width: 70px"
+    play.style = "width: 55px"
 
     play.teste = undefined
 
@@ -60,8 +60,62 @@ container.addEventListener("mouseover", function dentro(){
 container.addEventListener("mouseout", function fora(){
     let controle = document.getElementById("controle")
     controle.style.display = "none"
-
+    
 }) 
+
+
+
+
+let barraVolume = document.getElementById("barraVolume")
+barraVolume.addEventListener("input", function teste(){
+    video.volume = barraVolume.value
+
+    let BotaoVolume = document.getElementById("BotaoVolume")
+    if(barraVolume.value == 0){
+        BotaoVolume.src = "./imagens/mudo.png"
+    }else if(barraVolume.value >= 0.1 && barraVolume.value <= 0.4 ){
+        BotaoVolume.src = "./imagens/volumeBaixo.png"
+    }else if(barraVolume.value > 0.4 && barraVolume.value <= 0.7){
+        BotaoVolume.src = "./imagens/volumeMedio.png"
+    }else{
+        BotaoVolume.src = "./imagens/VolumeAlto.png"
+    }
+        
+    
+})
+
+
+function mudo(){
+    if(video.volume > 0){
+        barraVolume.value = 0.0
+        video.volume = 0
+        BotaoVolume.src = "./imagens/mudo.png"
+    }else{
+        video.volume = 0.5
+        barraVolume.value = 0.5
+        BotaoVolume.src = "./imagens/volumeMedio.png"
+    }
+    
+    
+    
+}
+
+
+
+let BotaoVolume = document.getElementById("BotaoVolume")
+BotaoVolume.addEventListener("mouseenter", function aparecerVolume(){
+    barraVolume.style.display = "inline"
+
+
+})
+
+
+function esconder(){
+    setTimeout(function esconderVolume(){
+    barraVolume.style.display = "none"
+    },3000)
+}
+
 
 
 let direita = document.getElementById("direita")
@@ -69,4 +123,6 @@ direita.addEventListener("dblclick", pular10)
 
 let esquerda = document.getElementById("esquerda")
 esquerda.addEventListener("dblclick", voltar10)
+
+
 
